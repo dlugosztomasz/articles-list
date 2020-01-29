c229d37aa64178a6072f44768649a81d5c69e0a6
Tomasz Długosz <tomekdlugos@gmail.com>
2020-01-29 19:02:40 +0100

    #initializing basic structure of the project

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: ['babel-polyfill', './src/index.js'],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: path.resolve('dist'),
+        publicPath: '/',
+        filename: 'main.js'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        contentBase: path.resolve('dist'),
+        hot: true,
+        port: 3000
+    }
+};
